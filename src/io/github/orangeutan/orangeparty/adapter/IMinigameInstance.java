93d3262625aa87c963d73b682f347d4f4e2ca7fc
OrangeUtan <Oran9eUtan@gmail.com>
2016-01-30 21:29:26 +0100

    Neuen Methode "canPlayerJoin" hinzugefügt und Rückgabewert der Methode "canPlayersJoin" geändert

--- src/io/github/orangeutan/orangeparty/adapter/IMinigameInstance.java
+++ src/io/github/orangeutan/orangeparty/adapter/IMinigameInstance.java
@@ -10,11 +10,18 @@ import java.util.Set;
 public interface IMinigameInstance {
 
     /**
-     * Check if the {@link Player}s can join the Minigame
-     * @param players
-     * @return
+     * Check if all the {@link Player} can join the Minigame
+     * @param player The {@link Player}s to check if they can join the Minigame
+     * @return true if the {@link Player} can join the Minigame, false otherwise
      */
-    boolean canPlayersJoin(Set<Player> players);
+    boolean canPlayerJoin(Player player);
+
+    /**
+     * Check if all the {@link Player}s in the {@link Set} can join the Minigame
+     * @param players The {@link Player}s to check if they can join the Minigame
+     * @return A {@link Set} of the {@link Player}s who can not join the Minigame
+     */
+    Set<Player> canPlayersJoin(Set<Player> players);
 
     /**
      * Add a {@link Player} to the Minigame
@@ -26,7 +33,7 @@ public interface IMinigameInstance {
     /**
      * Add all {@link Player}s in the {@link Set} to the Minigame
      * @param players The {@link Player}s which should be added to the Minigame
-     * @return A {@link Set} of Players who failed to join the Minigame
+     * @return A {@link Set} of the Players who failed to join the Minigame
      */
     Set<Player> joinAll(Set<Player> players);
 
@@ -40,7 +47,7 @@ public interface IMinigameInstance {
     /**
      * Remove all {@link Player}s in the {@link Set} from the Minigame
      * @param players The {@link Player}s who should be remove from the Minigame
-     * @return A {@link Set} of Players who couldn't be remove from the Minigame
+     * @return A {@link Set} of the Players who couldn't be remove from the Minigame
      */
     Set<Player> leaveAll(Set<Player> players);
 }
