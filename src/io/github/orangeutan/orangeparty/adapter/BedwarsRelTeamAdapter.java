f8bd38ddc0f72305c4d992ea713500a77e1ea25a
OrangeUtan <Oran9eUtan@gmail.com>
2016-01-31 15:04:52 +0100

    Neue Methoden "addAllPlayers" und "removeAllPlayers" entfernt

--- src/io/github/orangeutan/orangeparty/adapter/BedwarsRelTeamAdapter.java
+++ src/io/github/orangeutan/orangeparty/adapter/BedwarsRelTeamAdapter.java
@@ -3,6 +3,9 @@ package io.github.orangeutan.orangeparty.adapter;
 import io.github.yannici.bedwars.Game.Team;
 import org.bukkit.entity.Player;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Michael on 31.01.2016.
  */
@@ -16,15 +19,32 @@ public class BedwarsRelTeamAdapter implements IMinigameTeam {
         mTeam = team;
     }
 
-
     @Override
     public boolean addPlayer(Player player) {
         return mTeam.addPlayer(player);
     }
 
     @Override
+    public Set<Player> addAllPlayers(Set<Player> players) {
+        Set<Player> couldNotBeAdded = new HashSet<>();
+        for (Player player : players) {
+            if (!addPlayer(player)) couldNotBeAdded.add(player);
+        }
+        return couldNotBeAdded;
+    }
+
+    @Override
     public boolean removePlayer(Player player) {
         mTeam.removePlayer(player);
         return mTeam.isInTeam(player);
+    }
+
+    @Override
+    public Set<Player> removeAllPlayers(Set<Player> players) {
+        Set<Player> couldNotBeRemoved = new HashSet<>();
+        for (Player player : players) {
+            if (!removePlayer(player)) couldNotBeRemoved.add(player);
+        }
+        return couldNotBeRemoved;
     }
 }
--- src/io/github/orangeutan/orangeparty/adapter/IMinigameTeam.java
+++ src/io/github/orangeutan/orangeparty/adapter/IMinigameTeam.java
@@ -2,12 +2,38 @@ package io.github.orangeutan.orangeparty.adapter;
 
 import org.bukkit.entity.Player;
 
+import java.util.Set;
+
 /**
  * Created by Michael on 31.01.2016.
  */
 public interface IMinigameTeam {
 
+    /**
+     * Add a {@link Player} to the Team
+     * @param player The {@link Player} who is to be added to the Team
+     * @return True if the {@link Player} was added to the Team, false otherwise
+     */
     boolean addPlayer(Player player);
 
+    /**
+     * Add all the {@link Player}s in the {@link Set} to the Team
+     * @param players The {@link Player}s who are to be added to the Team
+     * @return A {@link Set} of the {@link Player}s who could not be added to the Team
+     */
+    Set<Player> addAllPlayers(Set<Player> players);
+
+    /**
+     * Remove a {@link Player} from the Team
+     * @param player The {@link Player} who is to be removed from the Team
+     * @return True if the {@link Player} was removed from the Team, false otherwise
+     */
     boolean removePlayer(Player player);
+
+    /**
+     * Remove all the {@link Player}s in the {@link Set} from the Team
+     * @param players The {@link Player}s who are to be remove from the Team
+     * @return A {@link Set} of the {@link Player}s who could not be removed from the Team
+     */
+    Set<Player> removeAllPlayers(Set<Player> players);
 }
